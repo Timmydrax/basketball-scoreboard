0c2572c214cda46959fda2ee11019656891a262e
Timmydrax <timmydrax@gmail.com>
2025-03-13 13:05:06 +0100

    CHORE: Did some refactoring

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-// Timer Section
-let timer = document.getElementById("timer");
+// DOM Elements
+const homeScore = document.getElementById("home-score");
+const guestScore = document.getElementById("guest-score");
+const homeFouls = document.getElementById("home-fouls");
+const guestFouls = document.getElementById("guest-fouls");
+const periodEl = document.getElementById("period");
+const timerEl = document.getElementById("timer");
+const homeTeam = document.getElementById("homeTeam");
+const guestTeam = document.getElementById("guest-team");
+
+// Timer Buttons
 let startTimer = document.getElementById("startTimer");
 let stopTimer = document.getElementById("stopTimer");
 let resetTimer = document.getElementById("resetTimer");
-// Period Section
-let period = document.getElementById("period");
+
+// Period Buttons
 let increasePeriod = document.getElementById("increasePeriod");
 let decreasePeriod = document.getElementById("decreasePeriod");
-// Team Section
-//Home
-let homeTeam = document.getElementById("homeTeam");
-let homeScore = document.getElementById("home-score");
+
+//Home Buttons
 let homeScoreOne = document.getElementsByName("home-score-1");
 let homeScoreTwo = document.getElementsByName("home-score-2");
 let homeScoreThree = document.getElementsByName("home-score-3");
 let homeScoreMinus = document.getElementsByName("home-score-minus");
-let homeFouls = document.getElementById("home-fouls");
 let homeFoulsIncrement = document.getElementById("home-fouls-plus");
 let homeFoulsDecrement = document.getElementById("home-fouls-minus");
-// Guest
-let guestTeam = document.getElementById("guest-team");
-let guestScore = document.getElementById("guest-score");
+
+//Guest Buttons
 let guestScoreOne = document.getElementsByName("guest-score-1");
 let guestScoreTwo = document.getElementsByName("guest-score-2");
 let guestScoreThree = document.getElementsByName("guest-score-3");
 let guestScoreMinus = document.getElementsByName("guest-score-minus");
-let guestFouls = document.getElementById("guest-fouls");
 let guestFoulsIncrement = document.getElementById("guest-fouls-plus");
 let guestFoulsDecrement = document.getElementById("guest-fouls-minus");
-// Reset Section
+
+// Reset buttons
 let resetGame = document.getElementById("new-game");
