0f82d773b08fda4b394ca7ae119927c113bfa4f5
Timmydrax <timmydrax@gmail.com>
2025-03-13 12:34:02 +0100

    NEW: Added core functionalities

---
+++ script.js
@@ -0,0 +1,32 @@
+// Timer Section
+let timer = document.getElementById("timer");
+let startTimer = document.getElementById("startTimer");
+let stopTimer = document.getElementById("stopTimer");
+let resetTimer = document.getElementById("resetTimer");
+// Period Section
+let period = document.getElementById("period");
+let increasePeriod = document.getElementById("increasePeriod");
+let decreasePeriod = document.getElementById("decreasePeriod");
+// Team Section
+//Home
+let homeTeam = document.getElementById("homeTeam");
+let homeScore = document.getElementById("home-score");
+let homeScoreOne = document.getElementsByName("home-score-1");
+let homeScoreTwo = document.getElementsByName("home-score-2");
+let homeScoreThree = document.getElementsByName("home-score-3");
+let homeScoreMinus = document.getElementsByName("home-score-minus");
+let homeFouls = document.getElementById("home-fouls");
+let homeFoulsIncrement = document.getElementById("home-fouls-plus");
+let homeFoulsDecrement = document.getElementById("home-fouls-minus");
+// Guest
+let guestTeam = document.getElementById("guest-team");
+let guestScore = document.getElementById("guest-score");
+let guestScoreOne = document.getElementsByName("guest-score-1");
+let guestScoreTwo = document.getElementsByName("guest-score-2");
+let guestScoreThree = document.getElementsByName("guest-score-3");
+let guestScoreMinus = document.getElementsByName("guest-score-minus");
+let guestFouls = document.getElementById("guest-fouls");
+let guestFoulsIncrement = document.getElementById("guest-fouls-plus");
+let guestFoulsDecrement = document.getElementById("guest-fouls-minus");
+// Reset Section
+let resetGame = document.getElementById("new-game");
