383570656ca2a945c965163f429e60e103ac83c1
Timmydrax <timmydrax@gmail.com>
2025-03-13 13:44:44 +0100

    FEAT: Added Game State feature

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // DOM Elements
-const homeScore = document.getElementById("home-score");
-const guestScore = document.getElementById("guest-score");
-const homeFouls = document.getElementById("home-fouls");
-const guestFouls = document.getElementById("guest-fouls");
-const periodEl = document.getElementById("period");
-const timerEl = document.getElementById("timer");
-const homeTeam = document.getElementById("homeTeam");
-const guestTeam = document.getElementById("guest-team");
+const homeScoreElement = document.getElementById("home-score");
+const guestScoreElement = document.getElementById("guest-score");
+const homeFoulsElement = document.getElementById("home-fouls");
+const guestFoulsElement = document.getElementById("guest-fouls");
+const periodElement = document.getElementById("period");
+const timerElement = document.getElementById("timer");
+const homeTeamElement = document.getElementById("homeTeam");
+const guestTeamElement = document.getElementById("guest-team");
 
 // Timer Buttons
 let startTimer = document.getElementById("startTimer");
@@ -35,3 +35,50 @@ let guestFoulsDecrement = document.getElementById("guest-fouls-minus");
 
 // Reset buttons
 let resetGame = document.getElementById("new-game");
+
+//Decalaring Game State Variables
+const gameState = {
+  homeScore: 0,
+  guestScore: 0,
+  homeFouls: 0,
+  guestFouls: 0,
+  period: 1,
+  timerMinutes: 12,
+  timerSeconds: 0,
+  timerRunning: false,
+  timerInterval: null,
+};
+
+// Displaying Initial Game State
+function updateScores() {
+  homeScoreElement.textContent = gameState.homeScore;
+  guestScoreElement.textContent = gameState.guestScore;
+}
+
+function updatePeriodElement() {
+  periodElement.textContent = gameState.period;
+}
+
+function updateHomeFouls() {
+  homeFoulsElement.textContent = gameState.homeFouls;
+}
+
+function updateGuestFouls() {
+  guestFoulsElement.textContent = gameState.guestFouls;
+}
+
+function updateTimerElement() {
+  const minutes = String(gameState.timerMinutes).padStart(2, "0");
+  const seconds = String(gameState.timerSeconds).padStart(2, "0");
+  timerElement.textContent = `${minutes}:${seconds}`;
+}
+
+function initializeGameStateDisplay() {
+  updateScores();
+  updatePeriodElement();
+  updateHomeFouls();
+  updateGuestFouls();
+  updateTimerElement();
+}
+
+initializeGameStateDisplay();
